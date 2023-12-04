7c1d10609e436d068139f37d9747db120aa28748
Kitanae <kadokawa.kitanae@gmail.com>
2023-12-04 15:52:51 +0900

    Line Renderer

---
+++ FermataSoft_Prototype/Assets/DemoAssets/Scripts/GridRenderer.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridRenderer : Graphic
+{
+
+    public Vector2Int gridSize = new Vector2Int(1, 1);
+
+    public float thickness = 10;
+
+    UIVertex vert;
+
+    float cellWidth;
+    float cellHeight;
+
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+
+        vh.Clear();
+
+        vert = UIVertex.simpleVert;
+
+        float width = rectTransform.rect.width;
+        float height = rectTransform.rect.height;
+
+        cellWidth = width / gridSize.x;
+        cellHeight = height / gridSize.y;
+
+        int count = 0;
+        for(int y=0; y<gridSize.y; y++)
+        {
+            for(int x=0; x<gridSize.x; x++)
+            {
+                DrawCell(x, y, count, vh);
+
+                count++;
+            };
+        }
+        /*
+        //Draw outer vertices
+        vert.position = new Vector3(0, 0);
+        vert.color = color;
+        vh.AddVert(vert);
+
+        vert.position = new Vector3(0, height);
+        vert.color = color;
+        vh.AddVert(vert);
+
+        vert.position = new Vector3(width, height);
+        vert.color = color;
+        vh.AddVert(vert);
+
+        vert.position = new Vector3(width, 0);
+        vert.color = color;
+        vh.AddVert(vert);
+
+        //Draw inner vertices
+        //The inner vertex is the diagonal position 
+        //So we do some pythag to know how much to move by in x and y
+
+        float widthSqr = thickness * thickness;
+        float distanceSqr = widthSqr / 2f;
+        float distance = Mathf.Sqrt(distanceSqr);
+
+        //Debug.Log($"{widthSqr}, {distanceSqr}, {distance}");
+
+        vert.position = new Vector3(distance, distance);
+        vert.color = color;
+        vh.AddVert(vert);
+
+        vert.position = new Vector3(distance, height-distance);
+        vert.color = color;
+        vh.AddVert(vert);
+
+        vert.position = new Vector3(width-distance, height-distance);
+        vert.color = color;
+        vh.AddVert(vert);
+
+        vert.position = new Vector3(width-distance, distance);
+        vert.color = color;
+        vh.AddVert(vert);
+
+        //Left Edge
+        vh.AddTriangle(0, 1, 5);
+        vh.AddTriangle(5, 4, 0);
+
+        //Top Edge
+        vh.AddTriangle(1, 2, 6);
+        vh.AddTriangle(6, 5, 1);
+
+        //Right Edge
+        vh.AddTriangle(2, 3, 7);
+        vh.AddTriangle(7, 6, 2);
+
+        //Bottom Edge
+        vh.AddTriangle(3, 0, 4);
+        vh.AddTriangle(4, 7, 3);
+        */
+    }
+
+    void DrawCell(int x, int y, int index, VertexHelper vh)
+    {
+
+        float xPos = cellWidth * x;
+        float yPos = cellHeight * y;
+
+        //Draw outer vertices
+        vert.position = new Vector3(xPos, yPos);
+        vert.color = color;
+        vh.AddVert(vert);
+
+        vert.position = new Vector3(xPos, yPos + cellHeight);
+        vert.color = color;
+        vh.AddVert(vert);
+
+        vert.position = new Vector3(xPos + cellWidth, yPos+cellHeight);
+        vert.color = color;
+        vh.AddVert(vert);
+
+        vert.position = new Vector3(xPos + cellWidth, yPos);
+        vert.color = color;
+        vh.AddVert(vert);
+
+        //Draw inner vertices
+        //The inner vertex is the diagonal position 
+        //So we do some pythag to know how much to move by in x and y
+
+        float widthSqr = thickness * thickness;
+        float distanceSqr = widthSqr / 2f;
+        float distance = Mathf.Sqrt(distanceSqr);
+
+        //Debug.Log($"{widthSqr}, {distanceSqr}, {distance}");
+
+        vert.position = new Vector3(xPos + distance, yPos + distance);
+        vert.color = color;
+        vh.AddVert(vert);
+
+        vert.position = new Vector3(xPos + distance, yPos + (cellHeight-distance));
+        vert.color = color;
+        vh.AddVert(vert);
+
+        vert.position = new Vector3(xPos + (cellWidth-distance), yPos + (cellHeight-distance));
+        vert.color = color;
+        vh.AddVert(vert);
+
+        vert.position = new Vector3(xPos + (cellWidth-distance), yPos + distance);
+        vert.color = color;
+        vh.AddVert(vert);
+
+        int count = index * 8;
+        //Left Edge
+        vh.AddTriangle(count + 0, count + 1,  count + 5);
+        vh.AddTriangle(count + 5, count + 4, count + 0);
+
+        //Top Edge
+        vh.AddTriangle(count + 1, count + 2, count + 6);
+        vh.AddTriangle(count + 6, count + 5, count + 1);
+
+        //Right Edge
+        vh.AddTriangle(count + 2, count + 3, count + 7);
+        vh.AddTriangle(count + 7, count + 6, count + 2);
+
+        //Bottom Edge
+        vh.AddTriangle(count + 3, count + 0, count + 4);
+        vh.AddTriangle(count + 4, count + 7, count + 3);
+    }
+
+}
---
+++ FermataSoft_Prototype/Assets/DemoAssets/Scripts/LineRenderer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode()]
+public class LineRenderer : Graphic
+{
+    public Vector2 gridSize;
+    public GridRenderer gridRenderer;
+
+    float totalWidth;
+    float totalHeight;
+
+    float unitWidth;
+    float unitHeight;
+
+    public float thickness;
+
+    public List<Vector2> points;
+
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+        vh.Clear();
+        
+        totalWidth = rectTransform.rect.width;
+        totalHeight = rectTransform.rect.height;
+
+        unitWidth = totalWidth / gridSize.x;
+        unitHeight = totalHeight / gridSize.y;
+
+        if (points.Count < 2) { return; }
+
+        float angle = 0;
+
+        for (int i=0; i<points.Count; i++)
+        {
+            Vector2 point = points[i];
+
+            if (i < points.Count - 1)
+            {
+                angle = GetAngle(points[i], points[i + 1]) + (45f); 
+            }
+
+            DrawVerticesForPoint(point, vh, angle);
+        }
+
+        for(int i=0; i < points.Count-1; i++)
+        {
+            int index = i * 2;
+            vh.AddTriangle(index + 0, index + 1, index + 3);
+            vh.AddTriangle(index + 3, index + 2, index + 0);
+        }
+
+    }
+
+    private void DrawVerticesForPoint(Vector2 point, VertexHelper vh, float angle)
+    {
+        UIVertex vertex = UIVertex.simpleVert;
+        vertex.color = color;
+
+        vertex.position = Quaternion.Euler(0, 0, angle) * new Vector2(-thickness / 2, 0);
+        vertex.position += new Vector3((unitWidth * point.x), unitHeight * point.y);
+        vh.AddVert(vertex);
+
+        vertex.position = Quaternion.Euler(0, 0, angle) * new Vector2(thickness / 2, 0);
+        vertex.position += new Vector3((unitWidth * point.x), unitHeight * point.y);
+        vh.AddVert(vertex);
+    }
+
+    public float GetAngle(Vector2 me, Vector2 target)
+    {
+        return (float)(Math.Atan2(target.y - me.y, target.x - me.x) * (180 / Math.PI));
+    }
+
+    private void Update()
+    {
+
+        bool dirty;
+
+        if (gridRenderer != null)
+        {
+            if (gridSize != gridRenderer.gridSize)
+            {
+                gridSize = gridRenderer.gridSize;
+                SetVerticesDirty();
+            }
+
+        }
+
+    }
+}
