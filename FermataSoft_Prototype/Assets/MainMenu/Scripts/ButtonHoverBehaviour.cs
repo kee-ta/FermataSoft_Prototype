0a14470e72dfb5e8d25b0438bfb1335e8d501889
Kitanae <kadokawa.kitanae@gmail.com>
2024-06-10 07:09:07 +0900

    Positiong tweek

--- FermataSoft_Prototype/Assets/MainMenu/Scripts/ButtonHoverBehaviour.cs
+++ FermataSoft_Prototype/Assets/MainMenu/Scripts/ButtonHoverBehaviour.cs
@@ -5,16 +5,23 @@ using UnityEngine.EventSystems;
 
 public class ButtonHoverBehaviour : MonoBehaviour, IPointerEnterHandler , IPointerExitHandler
 {
+    public bool hasSelector = false;
+    public float selectorOriginalY = .5f;
+    public GameObject selector;
     public float scaleSize= 1.5f;
     public float tweenTime = .5f;
     public void OnPointerEnter(PointerEventData eventData)
     {
         LeanTween.scale(this.gameObject, new Vector3(scaleSize,scaleSize,scaleSize),tweenTime);
+        if(hasSelector)
+        LeanTween.scale(selector, new Vector3(1,scaleSize,1),tweenTime);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         LeanTween.scale(this.gameObject, new Vector3(1,1,1),tweenTime);
+        if(hasSelector)
+        LeanTween.scale(selector, new Vector3(1,selectorOriginalY,1),tweenTime);
     }
 
     // Start is called before the first frame update
